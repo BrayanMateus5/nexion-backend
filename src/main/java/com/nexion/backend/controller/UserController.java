3c3e324d4e484a85b6f65f840c2e0c26e408a10f
BrayanMateus5 <mateusbrayan73@gmail.com>
2026-08-06 12:37:57 -0300

    feat(user): add User CRUD controller

--- src/main/java/com/nexion/backend/controller/UserController.java
+++ src/main/java/com/nexion/backend/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.nexion.backend.controller;
 
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nexion.backend.entity.User;
 import com.nexion.backend.service.UserService;
 
-@RestController
-@RequestMapping("/api/v1/users")
+@RestController // responde a requisição do HTTP com JSON
+@RequestMapping("/api/v1/users") // caminho de todos os métodos
 public class UserController {
 
     private final UserService service;
@@ -27,28 +25,28 @@ public class UserController {
         this.service = service;
     }
 
-    @PostMapping
+    @PostMapping // Criar
     public ResponseEntity<User> criar(@RequestBody User user) {
         User criado = service.criar(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(criado);
     }
 
-    @GetMapping
+    @GetMapping // buscar
     public ResponseEntity<List<User>> listarTodos() {
         return ResponseEntity.ok(service.listarTodos());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // buscar por ID
     public ResponseEntity<User> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}") // atualizar com ID
     public ResponseEntity<User> atualizar(@PathVariable Long id, @RequestBody User user) {
         return ResponseEntity.ok(service.atualizar(id, user));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}") // remove o ID
     public ResponseEntity<Void> remover(@PathVariable Long id) {
         service.remover(id);
         return ResponseEntity.noContent().build();
