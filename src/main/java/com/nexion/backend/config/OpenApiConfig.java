1534d398eef33869fbf7b016872fd3ef670838a1
BrayanMateus5 <mateusbrayan73@gmail.com>
2026-09-25 01:41:01 -0300

    docs(swagger): add OpenAPI config with JWT bearer scheme

---
+++ src/main/java/com/nexion/backend/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package com.nexion.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Nexion API", version = "v1", description = "API do sistema de gestão financeira do Nexion"), security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+public class OpenApiConfig {
+}
